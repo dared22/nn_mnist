29391f74cd6f59b84c94b03e176b6f05981a1ff4
LeoQuentin <leo.quentin.thorbjornsen.bakholt@nmbu.no>
2024-01-15 10:25:53 +0100

    added activation functions to layers and merged config_parser.py and model_factory.py into a single class

--- src/lowrank/layers/vanilla_low_rank.py
+++ src/lowrank/layers/vanilla_low_rank.py
@@ -1,13 +1,13 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.nn.init as init
 
 class VanillaLowRankLayer(nn.Module):
     """
-    A custom layer in PyTorch representing a vanilla low rank dense layer.
-    
-    This layer performs matrix multiplication in succession with three different weight matrices.
+    A custom PyTorch layer implementing a low-rank approximation of a dense layer.
+
+    It utilizes three weight matrices to approximate dense layer transformations, 
+    reducing parameter count. An optional activation function can be applied post-transformation.
 
     Parameters
     ----------
@@ -17,54 +17,45 @@ class VanillaLowRankLayer(nn.Module):
         The size of output.
     rank : int
         The rank of the low rank approximation.
+    activation : callable (optional)
+        The activation function to apply after the linear transformation
 
     Attributes
     ----------
     weight1 : torch.nn.Parameter
-        The first weight matrix of the layer with dimensions (input_size, rank). 
-        Initialized using He initialization suitable for ReLU activations.
+        The first weight matrix of the layer with dimensions (input_size, rank).
+        Initialized using He initialization.
     weight2 : torch.nn.Parameter
         The second weight matrix of the layer, a square matrix with dimensions (rank, rank).
-        Also initialized using He initialization.
+        Initialized using He initialization.
     weight3 : torch.nn.Parameter
-        The third weight matrix with dimensions (rank, output_size). Also initialized using He initialization
+        The third weight matrix with dimensions (rank, output_size). 
+        Initialized using He initialization.
     bias : torch.nn.Parameter
-        The bias parameter of the layer, with the same number of elements as the 
-        output_size. Initialized with random values from a standard normal distribution.
-        """
+        The bias parameter of the layer, with the same number of elements as the output_size.
+        Initialized with random values from a standard normal distribution.
+    """
 
-    def __init__(self, input_size, output_size, rank):
+    def __init__(self, input_size, output_size, rank, activation=None):
         super(VanillaLowRankLayer, self).__init__()
-        # Define the weight matrices
+        # Weight matricies
         self.weight1 = nn.Parameter(torch.Tensor(input_size, rank))
         self.weight2 = nn.Parameter(torch.Tensor(rank, rank))
         self.weight3 = nn.Parameter(torch.Tensor(rank, output_size))
-        
-        # Initialize the weights using He initialization (because we are mainly using ReLU)
+        # Initialize the weights
         init.kaiming_normal_(self.weight1, mode='fan_in', nonlinearity='relu')
         init.kaiming_normal_(self.weight2, mode='fan_in', nonlinearity='relu')
         init.kaiming_normal_(self.weight3, mode='fan_in', nonlinearity='relu')
-        
-        # Initialize the bias with random values
-        self.bias = nn.Parameter(torch.randn(output_size)) 
-   
+        # Bias
+        self.bias = nn.Parameter(torch.randn(output_size))
+        # Activation function
+        self.activation = activation
+
     def forward(self, x):
-        """
-        Defines the forward pass of the VanillaLowRankLayer.
-
-        Parameters
-        ----------
-        x : torch.Tensor
-            The input tensor.
-
-        Returns
-        -------
-        torch.Tensor
-            The output tensor after applying matrix multiplications with the weight matrices.
-        """
-        x = torch.matmul(x, self.weight1)  # First matrix multiplication
-        x = torch.matmul(x, self.weight2)  # Second matrix multiplication
-        x = torch.matmul(x, self.weight3)  # Third matrix multiplication
-        x = x + self.bias  # Add the bias
-
+        x = torch.matmul(x, self.weight1)
+        x = torch.matmul(x, self.weight2)
+        x = torch.matmul(x, self.weight3)
+        x = x + self.bias # Add the bias
+        if self.activation:
+            x = self.activation(x)
         return x
--- tests/test_config_utils/test_config_parser.py
+++ tests/test_config_utils/test_config_parser.py
@@ -1,7 +1,8 @@
 from lowrank.config_utils.config_parser import ConfigParser
+
 def test_parser():
-	config_parser = ConfigParser("/Users/leoquentin/Documents/Programmering/project-inf202/src/lowrank/config_utils/config_ex_ffn.toml")
 	config_parser = ConfigParser("tests/data/config_ex_ffn.toml")
 
 	config_parser.load_config()
-	assert config_parser.config['settings']['architecture'] == 'FFN'
+
+	assert config_parser.config is not None
