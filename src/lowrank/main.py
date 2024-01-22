a64c32cd5708e9f7da1e4fe5bb75b1a4b2a1cb32
dared22 <dared22@yandex.ru>
2024-01-23 00:20:22 +0100

    finished GUI.

--- src/lowrank/main.py
+++ src/lowrank/main.py
@@ -1,7 +1,3 @@
-from lowrank.training.trainer import Trainer
-from lowrank.training.neural_network import FeedForward
-from lowrank.training.MNIST_downloader import Downloader  # Ensure you have the correct import for Downloader
-from lowrank.predict import predict, show_image
 import customtkinter as ctk
 from lowrank.training.gui import GUI
 
@@ -12,17 +8,3 @@ app.mainloop()
 
 
 
-#path =  './data/trained_model.pt'
-#NeuralNet.export_model(trained_nn,path)
-#
-#
-#
-#
-#
-## Load the trained model
-#trained_model = NeuralNet  # Creating an instance of the model
-#NeuralNet.import_model(trained_model, path) # Loading the trained weights into the model
-#trained_model.eval()  # Setting the model to evaluation mode
-#
-#
-## Predict numbers from MNIST dataset
--- src/lowrank/training/gui.py
+++ src/lowrank/training/gui.py
@@ -1,6 +1,5 @@
 from lowrank.training.trainer import Trainer
 from lowrank.training.neural_network import FeedForward
-from lowrank.training.MNIST_downloader import Downloader  # Ensure you have the correct import for Downloader
 from lowrank.predict import predict, show_image
 import customtkinter as ctk
 from tkinter import filedialog
@@ -8,11 +7,9 @@ import torch
 import tkinter.scrolledtext as st
 import sys
 import threading
-import tkinter as tk
-from PIL import Image, ImageDraw, ImageOps
+from PIL import Image, ImageOps
 import io
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import glob
 
@@ -28,10 +25,11 @@ class GUI:
 
         # Initialize variables
         self._selected_filename = None
-        self._nn = None
 
         # Create and place widgets
         self.create_widgets()
+        # Create NeuralNet
+        self._NeuralNet = FeedForward.create_from_config(self.browse_files("Select a configfile"))
 
     class TextRedirector(object):
         def __init__(self, widget):
@@ -42,7 +40,7 @@ class GUI:
             self.widget.see(ctk.END)
             self.widget.update_idletasks()
 
-        def flush(self):
+        def flush(self): #AttributeError: 'TextRedirector' object has no attribute 'flush'
             pass
 
     def create_widgets(self):
@@ -50,11 +48,11 @@ class GUI:
         self.label_file_explorer = ctk.CTkLabel(self.app, text="File Explorer", width=100, height=40, fg_color="gray", text_color="white")
         self.label_file_explorer.grid(row=0, column=0, columnspan=2, pady=10, padx=10, sticky="ew")
 
-        self.button_explore = ctk.CTkButton(self.app, text="Browse Files", command=self.browse_files)
-        self.button_explore.grid(row=1, column=0, pady=10, padx=10, sticky="ew")
+        self.save_btn = ctk.CTkButton(self.app, text="Save Trained Model", command=self.save_model)
+        self.save_btn.grid(row=3, column=1, pady=10, padx=10, sticky="ew")
 
         self.button_training = ctk.CTkButton(self.app, text="Train NN", command=self.start_training_thread)
-        self.button_training.grid(row=1, column=1, pady=10, padx=10, sticky="ew")
+        self.button_training.grid(row=1, column=0, pady=10, padx=10, sticky="ew")
 
         self.button_predict = ctk.CTkButton(self.app, text="Predict a Number from mnist", command=self.predict_btn)
         self.button_predict.grid(row=2, column=0, pady=10, padx=10, sticky="ew")
@@ -66,7 +64,7 @@ class GUI:
         self.load_model_btn.grid(row=3, column=0, pady=10, padx=10, sticky="ew")
 
         self.feature_btn = ctk.CTkButton(self.app, text='Leo`s Feature', command=self.feature)
-        self.feature_btn.grid(row=3, column=1, pady=10, padx=10, sticky="ew")
+        self.feature_btn.grid(row=1, column=1, pady=10, padx=10, sticky="ew")
 
         self.output_area = st.ScrolledText(self.app, height=10)
         self.output_area.grid(row=5, column=0, columnspan=2, padx=10, sticky="ew")
@@ -87,31 +85,27 @@ class GUI:
             return toml_files
         paths = list_toml_files(choose_folder())
         for path in paths:
-            nns.append(self.train_nn(path))
-
-        
-
-
-
+            nn = FeedForward.create_from_config(path)
+            nns.append(self.train_nn(nn))
 
     # Function to train the neural network
-    def train_nn(self,path):
-        NeuralNet = FeedForward.create_from_config(path)
+    def train_nn(self, nn):
         trainer = Trainer() 
-        trained_nn = trainer.train(NeuralNet)
-        self._nn = trained_nn
+        trained_nn = trainer.train(nn)
+        self._NeuralNet = trained_nn
         return trained_nn
 
     # Function to start training in a separate thread
     def start_training_thread(self):
-        training_thread = threading.Thread(target=self.train_nn, args=(self._selected_filename,))
+        training_thread = threading.Thread(target=self.train_nn, args=(self._NeuralNet,))
         training_thread.start()
 
     # Function to browse files
-    def browse_files(self):
-        filename = filedialog.askopenfilename(initialdir="/", title="Select a File")
+    def browse_files(self, title):
+        filename = filedialog.askopenfilename(initialdir="/", title=title)
         self.label_file_explorer.configure(text="File Opened: " + filename)
         self._selected_filename = filename
+        return filename
 
     def predict_btn(self):
         # Function to handle the prediction logic
@@ -121,8 +115,8 @@ class GUI:
             print(f"Predicting with input: {input_value}")
             pop_up.destroy()  # Close the pop-up window after prediction
                 # Predict numbers from MNIST dataset
-            prediction, label = predict(self._nn, input_value)
-            print(f'Model predicts:{prediction} and the actual number is {label}')
+            prediction, label = predict(self._NeuralNet, input_value)
+            print(f'Model predicts: {prediction} and the actual number is {label}')
 
         # Create a pop-up window
         pop_up = ctk.CTkToplevel(self.app)
@@ -137,11 +131,17 @@ class GUI:
         predict_button.pack(pady=10)
 
     def load_model(self):
-        NeuralNet = FeedForward.create_from_config("tests/data/config_ex_ffn.toml")
-        trained_model = NeuralNet
-        NeuralNet.import_model(trained_model, self._selected_filename) # Loading the trained weights into the model
+        trained_model = self._NeuralNet
+        self._NeuralNet.import_model(trained_model, self.browse_files("Select a model to load")) # Loading the trained weights into the model
         trained_model.eval()
-        self._nn = trained_model
+        self._NeuralNet = trained_model
+        
+    def save_model(self):
+        def choose_folder():
+            filename = filedialog.askdirectory(initialdir="/", title="Select a Folder")
+            return filename
+        self._NeuralNet.export_model(self._NeuralNet, f'{choose_folder()}/trained_model.pt')
+
 
     def open_drawing_window(self):
         def start_paint(event):
@@ -153,7 +153,7 @@ class GUI:
             """Draw the line on the canvas."""
             global last_x, last_y
             x, y = event.x, event.y
-            canvas.create_line((last_x, last_y, x, y), width=10, fill='black')
+            canvas.create_line((last_x, last_y, x, y), width=15, fill='black')
             last_x, last_y = x, y
 
         # Create a pop-up window
@@ -161,23 +161,24 @@ class GUI:
         pop_up.title("Draw a Number")
 
         # Create a canvas widget for drawing
-        canvas = tk.Canvas(pop_up, width=280, height=280, bg='white')
+        canvas = ctk.CTkCanvas(pop_up, width=280, height=280, bg='white')
         canvas.pack(pady=20, padx=20)
 
         def save_canvas():
             img = get_image_from_canvas(canvas, 28, 28)  # Resize to 28x28 for MNIST
             img = process_image(img)
             tensor = image_to_tensor(img)
-            self._nn.eval()
+            self._NeuralNet.eval()
             with torch.no_grad():
-                output = self._nn(tensor)
+                output = self._NeuralNet(tensor)
                 prediction = output.argmax()  # Getting the most likely class
-            print(f'Model predicts:{prediction.item()}')
+            print(f'Model predicts: {prediction.item()}')
+            canvas.delete("all")
 
 
 
-        save_btn = tk.Button(pop_up,text='save', command=save_canvas)
-        save_btn.pack()
+        save_btn = ctk.CTkButton(pop_up,text='Predict', command=save_canvas, width=280)
+        save_btn.pack(padx=20, pady=20)
 
         # Bind mouse events to the canvas
         canvas.bind('<Button-1>', start_paint)
@@ -207,5 +208,5 @@ class GUI:
             tensor = tensor.unsqueeze(0)  # Add batch dimension, if necessary
             return tensor
         
-    sys.stdout = sys.__stdout__
+    sys.stdout = sys.__stdout__ #get output back to console
 
