8c7f6299f4a6818b27f2e6ae1ceb901498e1b2df
dared22 <dared22@yandex.ru>
2024-01-24 13:16:46 +0100

    added functionality to cli

--- src/lowrank/main.py
+++ src/lowrank/main.py
@@ -10,13 +10,15 @@ from lowrank.config_utils.config_parser import ConfigParser
 from torch.utils.data import DataLoader
 
 class NNTrainerCLI:
-    def __init__(self, config_path):
-        self.config_path = config_path
+    def __init__(self, config_path=None):
+        self.config_path = config_path or 'config.toml'
         self._trainloader = None
         self._testloader = None
         self._NeuralNet = None
-        self.load_config()
-        self.download_data()
+        self.batchSize = None
+        if config_path:
+            self.load_config()
+            self.download_data()
 
     def load_config(self):
         if not os.path.isfile(self.config_path):
@@ -38,6 +40,25 @@ class NNTrainerCLI:
         self._NeuralNet = trained_nn
         print("Training completed. Log: ", training_log)
 
+    def train_all_from_folder(self, folder_path):
+        models = FeedForward.mass_create_models(folder_path)
+        for filename, model in models.items():
+            self._NeuralNet = model
+
+            self.batchSize = model.config_parser.batch_size
+            self.download_data()
+
+            trainer = Trainer.create_from_model(model)
+            trained_model, training_log = trainer.train(self._trainloader, self._testloader)
+            model_save_path = f'./data/mass_created_model_from_file_{filename}.pt'
+            torch.save(trained_model.state_dict(), model_save_path)
+            print(f'Finished training for model from file {filename}, training log: {training_log}')
+
+            
+
+
+
+
 def launch_gui():
     app = ctk.CTk()
     gui = GUI(app)
@@ -50,10 +71,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train a neural network with a specified configuration file.")
     parser.add_argument('--config', type=str, default=default_config_path, help=f"Path to the TOML configuration file (default: {default_config_path})")
     parser.add_argument('--gui', action='store_true', help="Launch the GUI instead of CLI")
+    parser.add_argument('--train_all', type=str, help="Train all models from a specified folder")
     args = parser.parse_args()
 
     if args.gui:
         launch_gui()
+    elif args.train_all:
+        trainer_cli = NNTrainerCLI()
+        trainer_cli.train_all_from_folder(args.train_all)
     else:
         try:
             nn_trainer = NNTrainerCLI(args.config)
